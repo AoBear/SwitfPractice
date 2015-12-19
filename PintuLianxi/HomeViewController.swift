c12ade99e467eca0376eeb4e6a5c5c0c8e441d80
AoBear <18968091680@163.com>
2015-12-19 15:14:07 +0800

    storyboard

---
+++ PintuLianxi/HomeViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  PintuLianxi
+//
+//  Created by DemonChou on 15/12/19.
+//  Copyright © 2015年 DemonChou. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    @IBOutlet weak var startGame: UIButton!
+    
+    @IBOutlet weak var OtherGame: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
